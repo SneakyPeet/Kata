bce71f0796ce638454921f5deb07af80e02ad444
Pieter Koornhof <pieter@entelect.co.za>
2015-05-20 22:04:49 +0200

    Add String Calculator Kata

---
+++ Kata/StringCalculator/Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Kata.StringCalculator
+{
+    public class Calculator //http://osherove.com/tdd-kata-1/
+    {
+        public int Add(string input)
+        {
+            return string.IsNullOrEmpty(input) ? 0 : AddStringNumbers(input);
+        }
+
+        private static int AddStringNumbers(string input)
+        {
+            var numbers = ExtractNumbersAsList(input);
+            GuardAgainstNegativeNumbers(numbers);
+            return numbers.Where(LessThan1000()).Sum();
+        }
+
+        private static List<int> ExtractNumbersAsList(string input)
+        {
+            var numbers = new List<int>();
+            var delimiters = new []{","};
+            using(var reader = new StringReader(input))
+            {
+                var line = reader.ReadLine();
+                if(HasDefinedDelimiters(line))
+                {
+                    delimiters = ExtractDelimiters(line);
+                    line = reader.ReadLine();
+                }
+                while(line != null)
+                {
+                    numbers.AddRange(ExtractNumbersFromLine(line, delimiters));
+                    line = reader.ReadLine();
+                }
+            }
+            return numbers;
+        }
+
+        private static bool HasDefinedDelimiters(string line)
+        {
+            return line.StartsWith("//");
+        }
+
+        private static string [] ExtractDelimiters(string line)
+        {
+            var delimiter = line.Substring(2).Replace("\n", string.Empty);
+            var match = Regex.Matches(delimiter, "\\[(.*?)\\]"); ;
+            return match.Count > 0 ? ExtractDelimiters(match) : new[] { delimiter };
+        }
+
+        private static string[] ExtractDelimiters(MatchCollection match)
+        {
+            var delimiters = new List<string>();
+            foreach(var del in match)
+            {
+                delimiters.Add(del.ToString().Trim(new []{'[',']'}));
+            }
+            return delimiters.ToArray();
+        }
+
+        private static IEnumerable<int> ExtractNumbersFromLine(string line, string[] delimiters)
+        {
+            var split = line.Split(delimiters, StringSplitOptions.None);
+            return split.Select(Int32.Parse);
+        }
+
+        private static void GuardAgainstNegativeNumbers(IEnumerable<int> numbers)
+        {
+            var negatives = numbers.Where(x => x < 0).ToList();
+            if(negatives.Any())
+            {
+                throw new ArgumentException(string.Join(",",negatives));
+            }
+        }
+
+        private static Func<int, bool> LessThan1000()
+        {
+            return x => x < 1000;
+        }
+    }
+}
---
+++ Kata/StringCalculator/Tests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Kata.StringCalculator
+{
+    [TestFixture]
+    class Tests //http://osherove.com/tdd-kata-1/
+    {
+        private Calculator calculator;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void GivenEmptyString_ReturnZero()
+        {
+            var result = calculator.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void GivenNumberString_ReturnNumber()
+        {
+            var result = calculator.Add("1");
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void GivenCommaSeperatedNumberString_ReturnSumOfNumbers()
+        {
+            var result = calculator.Add("1,2,3");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void GivenNewLineSeperatedNumberString_ReturnSumOfNumbers()
+        {
+            var result = calculator.Add("1\n2,3");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void GivenAProvidedDelimiterOnly_ReturnZero()
+        {
+            var result = calculator.Add("//;\n");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void GivenAProvidedDelimiterWithSingleNumber_ReturnNumber()
+        {
+            var result = calculator.Add("//;\n2");
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void GivenAProvidedDelimiterWithArrayOfNumbers_ReturnTotal()
+        {
+            var result = calculator.Add("//;\n1;2;3");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        [TestCase("-1","-1")]
+        [TestCase("2\n-5", "-5")]
+        [TestCase("//*\n-1*5*-3", "-1,-3")]
+        public void GivenNegativeNumbers_ThrowExceptionWithNumber(string testcase, string expectedException)
+        {
+            try
+            {
+                calculator.Add(testcase);
+            }
+            catch(ArgumentException ae)
+            {
+                Assert.AreEqual(expectedException, ae.Message);
+                return;
+            }
+            Assert.Fail("Exception Was Not Thrown");
+        }
+
+        [Test]
+        [TestCase("1000", 0)]
+        [TestCase("2\n5000", 2)]
+        [TestCase("//*\n1*5*3000", 6)]
+        public void GivenHighNumbers_IgnoreNumbers(string testcase, int expectedTotal)
+        {
+            var result = calculator.Add(testcase);
+            Assert.AreEqual(expectedTotal, result);
+        }
+
+        [Test]
+        public void GivenDelimiterOfAnyLength_ReturnSumOfNumbers()
+        {
+            var result = calculator.Add("//[***]\n1***5***3");
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        [TestCase("//[*][%]\n1*2%3", 6)]
+        [TestCase("//[***][%]\n1***2%3", 6)]
+        public void GivenMultipleDelimiters_ReturnSumOfNumbers(string testcase, int expectedTotal)
+        {
+            var result = calculator.Add(testcase);
+            Assert.AreEqual(expectedTotal, result);
+        }
+}
+}
